5aa807bd86bfca0a4f932027d4ec51c5f22f8408
henriquethuler <henriquethuler@gmail.com>
2026-08-27 17:54:13 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
+    base: './', // Adicione esta linha para corrigir caminhos relativos
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
